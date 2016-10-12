808e9fbdb14f805fafcbfcd2973e80e26b64039b
luifeifei <1805008320@qq.com>
2016-10-12 12:48:32 +0800

    firstjava

---
+++ Number.java
@@ -0,0 +1,13 @@
+public class Number{ //创建类
+
+	public static void main (String[] args) {//主方法
+
+		byte mybyte = 124;//声明byte类变量mybyte并赋值
+		short myshort = 32464;//声明short变量myshort并赋值
+		int myint = 4568912;//声明int变量myint并赋值
+		long mylong = 45678951;//声明long类型变量mylong并赋值
+		long result = mybyte + myshort + myint + mylong;//获得个变量的和
+		System.out.println("结果为："+result);
+	
+	}
+}
