4c0d667ddce937ed723d03a42a1ffa740c3da203
luifeifei <1805008320@qq.com>
2016-10-12 23:42:03 +0800

    java

---
+++ F.java
@@ -0,0 +1,30 @@
+/**自加自减*/
+public class F {//创建类
+
+	public static void main(String[] args) {//主方法
+		
+		int i = 0;								//声明int变量i并赋值
+		int j = 0;							  	//声明int变量j并赋值
+		System.out.println(i);					//输出i的值
+		System.out.println(j);					//输出j的值
+		
+		i++;									//自加i
+		j++;									//自加j
+		
+		System.out.println(i);					//输出自加后idea值
+		System.out.println(j);					//输出自加后j的值
+		
+		System.out.println("------------------------");
+		System.out.println(i++);				//输出i++
+		System.out.println(++j);				//输出++j
+		
+		
+		
+		System.out.println("--------------------------");
+		System.out.println(i);					//输出自加后i的值
+		System.out.println(j);					//输出自加后j的值
+		
+		
+	}
+
+}
